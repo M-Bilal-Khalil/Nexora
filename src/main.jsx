93b3f9d09e6896062e2994e0c678969416f015b5
M-Bilal-Khalil <im.bilalkhalil@gmail.com>
2025-10-11 21:26:57 +0500

    publish

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Nexora-Store">
+    <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nexora-Store/', // <-- 👈 your repo name here
+  base: '/Nexora-Store/', 
 })
