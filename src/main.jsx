97826f06010c907d0944b8ac075f4414a7bb82e8
M-Bilal-Khalil <im.bilalkhalil@gmail.com>
2025-10-18 09:41:35 +0500

    Add netlify redirects

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { CartProvider } from './context/CartContext';
-import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
