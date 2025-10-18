ae09c5be666c19255a59123a38b65393b24a79fc
M-Bilal-Khalil <im.bilalkhalil@gmail.com>
2025-10-18 09:46:07 +0500

    Trying deployment again

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace "M-Bilal-Khalil" with your GitHub username and "Nexora" with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/Nexora/', 
+  base: '/', // 👈 Important for Netlify
 })
